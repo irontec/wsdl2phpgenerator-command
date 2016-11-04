3766b7b8869090776b7e21fa14c1992cc31d4520
Javier Infante <jabi@irontec.com>
2016-11-04 09:10:29 +0100

    first try

---
+++ Command/ParserCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Wsdl2PhpGeneratorCliBundle\Command;
+
+use Psr\Log\LogLevel;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\OutputInterface;
+use Wsdl2PhpGenerator\Config;
+use Wsdl2PhpGenerator\Generator as Generator;
+
+
+class ParserCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('wsdl2php_generator_cli:parser')
+            ->setDescription('Parse remote WSDL, and create PHP Classes .')
+            ->addOption(
+                'wsdl',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'The WSDL URL.'
+            )
+            ->addOption(
+                'output',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Output Directory for generated classes'
+            )
+            ->addOption(
+                'ns',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Class Namespace Class\\Example'
+            );
+
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $verbosityLevelMap = array(
+            LogLevel::INFO   => OutputInterface::VERBOSITY_VERY_VERBOSE,
+        );
+        $logger = new ConsoleLogger($output, $verbosityLevelMap);
+
+
+        $generator = new Generator();
+        $generator->setLogger($logger);
+        $generator->generate(
+            new Config(array(
+                'inputFile' => $input->getOption('wsdl'),
+                'outputDir' => $input->getOption('output'),
+                'namespaceName' => $input->getOption('ns'),
+                'sharedTypes' => true,
+                'constructorParamsDefaultToNull' => true
+            ))
+        );
+
+
+    }
+}
---
+++ Wsdl2PhpGeneratorCliBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wsdl2PhpGeneratorCliBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Wsdl2PhpGeneratorCliBundle extends Bundle
+{
+}
