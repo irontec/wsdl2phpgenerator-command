a29c3a45ceabcd16a6baf50afd25d145ae9eef81
Natalia Aznar <naznar@irontec.com>
2018-12-26 16:01:43 +0100

    Register service

---
+++ DependencyInjection/Wsdl2PhpGeneratorCommandExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Irontec\Bundle\Wsdl2PhpGeneratorCommandBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Console\Application;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class Wsdl2PhpGeneratorCommandExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        if (class_exists(Application::class)) {
+            $loader->load('console.xml');
+        }
+    }
+
+
+}
